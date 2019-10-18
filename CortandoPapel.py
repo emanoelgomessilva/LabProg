7e2174522137401ae4f80955dcf9b5bb9cedb9f2
Emanoel Gomes <emanoel20092009@gmail.com>
2019-10-18 09:41:24 -0300

    Entregas 18/10/19 organizadas

--- CortandoPapel.py
+++ CortandoPapel.py
@@ -1,26 +1,38 @@
 n = input()
+#Lista que guarda os retangulos
 retangulos = input().split(" ")
+#Lista que guarda os retangulos com menor altura
 vales = []
+#Variável que guarda a maior quantidade de cortes possível
 maiorDivisao = -1
 
+#Se o primeiro retângulo da sequência for menor do que o seu sucessor, ele é um vale
 if(int(retangulos[0])<int(retangulos[1])):
     vales.append(int(retangulos[0]))
 
+#Se o ultimo retângulo da sequência for menor do que o seu antecessor, ele é um vale
 if(int(retangulos[int(n)-1])<int(retangulos[int(n)-2])):
     vales.append(int(retangulos[int(n)-1]))
 
+#Para cada retangulo da sequencia, se o seu antecessor e sucessor forem maiores que ele, ele será um vale
 for i in range(len(retangulos)-1):
     if(i-1 >= 0 and i+1<= len(retangulos)-1):
         if(int(retangulos[i-1])>int(retangulos[i]) and int(retangulos[i+1])>int(retangulos[i])):
             vales.append(int(retangulos[i]))
-
+#
 for vale in vales:
+    #Inicialmente haverá um pedaço.
     divisao = 1
+    #É assumido que se o primeiro retangulo da sequencia for maior que o vale testado, já será acrescentado,
+    #um novo pedaço à divisão.
     if (int(retangulos[0])>vale):
         divisao+=1
+    #Os retângulos são percorridos. Se um retangulo for menor ou igual ao vale percorrido, será contabilizado
+    #um novo pedaço à divisão
     for i in range(len(retangulos)-1):
         if(int(retangulos[i])<=vale and int(retangulos[i-1])>vale):
             divisao+=1
+    #Se a divisão resultante for maior ao valor corrente de maiorDivisão, ela será a maior divisão
     if(divisao>maiorDivisao):
         maiorDivisao = divisao
 
--- DividindoImperio_TimeTry.py
+++ DividindoImperio_TimeTry.py
@@ -1,8 +1,9 @@
 import math
 
+#Classe que rerpresenta uma cidade do imperio
 class Cidade:
     def __init__(self):
-        #Código da província
+        #Código da cidade
         self.codigo = 0
         #Conjunto de arestas que ligam o vértice à seus vizinhos
         self.vizinhos = []
@@ -19,16 +20,27 @@ class Cidade:
     def setCodigo(self,codigo):
         self.codigo = codigo
 
+#Função utilizada para mapear as cidades que farão parte do novo império dividido, ela recebe como parâmetros, a
+#primeira cidade que será percorrida, a estrada que será removida, o mapa com todas as cidades do império atual e uma
+# lista que armazenará as cidades que farão parte do novo império
 def mapeiaCidades(inicio,estrada, mapa,cidades):
     vizinhos = mapa[inicio].getVizinhos()
+    #A partir de inicio, serão percorridos de forma recursiva os seus vizinhos
     for vizinho in vizinhos:
+        # Se o vizinho não estiver nas cidades ou ele for igual ao destino da estrada removida
         if ((vizinho not in cidades) or (vizinho == int(estrada[1]))):
+            #Se a cidade de inicio for diferente da origem da estrada bloqueada ou a sua cidade vizinha for diferente
+            #do destino da estrada removida, significa que há um estrada disponível entre as duas cidades
             if((inicio+1 != int(estrada[0]) or vizinho != int(estrada[1]))):
+                #Se o vizinho não estiver no percurso da estrada que foi removida e não estiver registrado nas cidades
+                #do novo imperio, ele será inserido novo imperio, caso contrário, se a cidade inicio testada na recursão
+                #não estiver nas cidades do novo imperio, ela será inserida na lista das novas cidades
                 if vizinho not in cidades:
                     if vizinho != int(estrada[0]) and vizinho != int(estrada[1]):
                         cidades.append(vizinho)
                     elif inicio+1 not in cidades:
                         cidades.append(inicio+1)
+                    #Em seguida será feita uma chamada recursiva do procedimento para o vizinho, sendo ele o novo inicio
                     mapeiaCidades(vizinho-1, estrada, mapa, cidades)
             else:
                 break;
@@ -58,6 +70,11 @@ for estrada in estradas:
     vizinhancaB.append(a)
     mapa[b-1].setVizinhos(vizinhancaB)
 
+#Será feita a execução do procedimento de mapearCidades para cada possibilidade de estrada removida, a quantidade de
+#cidades do primeiro imperio será a quantidade de cidades retornada do procedimento e a quantidade de cidades do segundo
+#imperio será igual a diferença entre a quantidade de cidades total do imperio atual e a quantidade de cidades
+#resultantes do imperio atual. após isso é calculada a diferença entre a quantidade de cidades dos dois imperios e se
+#essa diferença for menor do que a menor diferença encontrada anteriormente ela será a nova menor diferença.
 for estrada in estradas:
     cidades = []
     imperio1 = mapeiaCidades(0, estrada, mapa, cidades)
